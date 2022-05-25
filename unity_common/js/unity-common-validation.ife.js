c92b3846023fc6f36fad1731254cf772c8f34936
Adam Williamson <adam.williamson@finance-ni.gov.uk>
2022-05-25 11:07:10 +0100

    Amending site search error html to use p

---
+++ unity_common/js/unity-common-validation.ife.js
@@ -0,0 +1,41 @@
+/**
+ * @file
+ * Attaches behaviors for the Clientside Validation jQuery module.
+ */
+
+(function ($, drupalSettings) {
+
+  'use strict';
+
+  // Disable clientside validation for webforms submitted using Ajax.
+  // This prevents Computed elements with Ajax from breaking.
+  // @see
+  // \Drupal\clientside_validation_jquery\Form\ClientsideValidationjQuerySettingsForm
+  drupalSettings.clientside_validation_jquery.validate_all_ajax_forms = 0;
+
+  /**
+   * Add .cv-validate-before-ajax to all webform submit buttons.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.errorMessageFixAjax = {
+    attach: function (context) {
+      $('form.search-form--site .form-actions .js-form-submit')
+        .once('error-message-fix-ajax')
+        .addClass('cv-validate-before-ajax');
+    }
+  };
+
+  $(document).once('errormessage-fix').on('cv-jquery-validate-options-update', function (event, options) {
+    options.errorElement = 'p';
+    options.showErrors = function (errorMap, errorList) {
+      // Show errors using defaultShowErrors().
+      this.defaultShowErrors();
+
+      // Add '.form-item--error-message' class to all errors.
+      $(this.currentForm).find('strong.error').addClass('form-item--error-message');
+
+    };
+  });
+
+})(jQuery, drupalSettings);
