63bb661e3c65932118629769c2313be47c7b821d
Adam Williamson <adam.williamson@finance-ni.gov.uk>
2023-06-21 15:56:48 +0100

    Tidying use statements

--- unity_file_migrations/src/Plugin/migrate/process/MediaWysiwygFilter.php
+++ unity_file_migrations/src/Plugin/migrate/process/MediaWysiwygFilter.php
@@ -2,8 +2,8 @@
 
 namespace Drupal\unity_file_migrations\Plugin\migrate\process;
 
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\MigrateExecutableInterface;
 use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
