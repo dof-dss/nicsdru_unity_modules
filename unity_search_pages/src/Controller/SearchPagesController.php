7b2e27fdea82417903696f8c9c77eb3175b35575
Johan Gant <451261+johangant@users.noreply.github.com>
2026-09-24 15:34:21 +0100

    D11 module compatibility

--- unity_search_pages/src/Controller/SearchPagesController.php
+++ unity_search_pages/src/Controller/SearchPagesController.php
@@ -76,7 +76,7 @@ class SearchPagesController extends ControllerBase implements ContainerInjection
     if ($route === NULL) {
       $route = $this->routeMatch->getRouteName();
     }
-    $facet = $this->request->get('facets_query');
+    $facet = $this->request->query->get('facets_query');
 
     $title = $this->getTitleFromRoute($route);
     $search = $this->request->query->all();
